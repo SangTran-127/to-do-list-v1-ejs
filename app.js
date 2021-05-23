1aea3fc4980a301fbf29592a6788eafb7a3f7ffb
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-23 22:23:23 +0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('hello')
 })
+const today = new Date().getDay()
+console.log(today);
 app.listen(port, () => {
     console.log(`sever is running on port ${port}`);
 })
